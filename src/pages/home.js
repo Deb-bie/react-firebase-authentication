e386f18c6e6f6b1405d003e60d7b581e7b0a1f9b
unknown <Deb-bie>
2021-12-17 04:32:40 +0000

    Designed the homepage

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,10 +1,23 @@
-import React from 'react'
+import React from 'react';
+import video from '../components/video/vid.mp4'
+import '../App.css'
 
 const Home = () => {
     return (
-        <div>
-            Home
+        
+        <div className="main-container" id="home">
+            <div className="main-background">
+                <video className="video-background" autoPlay loop muted src={video} typeof="video/mp4"></video>
+            </div>
+
+
+            <div className="main-content">
+                <h1 className="title">
+                    Welcome Home
+                </h1>
+            </div>
         </div>
+        
     )
 }
 
